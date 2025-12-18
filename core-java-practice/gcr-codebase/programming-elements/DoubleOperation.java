97e24531e5ac5ecfb39c5f4c32074becdceef7fb
Ayush Alapuriya <alapuriya0@gmail.com>
2025-12-18 22:25:16 +0530

    Implemented basic java problems

---
+++ core-java-practice/gcr-codebase/programming-elements/DoubleOperation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class DoubleOperation {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		double a=sc.nextDouble();
+		double b=sc.nextDouble();
+		double c=sc.nextDouble();
+		double opr1=a+b*c;
+		double opr2=a*b+c;
+		double opr3=a+b/c;
+		double opr4=a%b+c;
+		System.out.println("The results of Int Operations are "+opr1+" , "+opr2+" , "+opr3+" and "+opr4);
+	}
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/IntOperations.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class IntOperations {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int opr1=a+b*c;
+		int opr2=a*b+c;
+		int opr3=a+b/c;
+		int opr4=a%b+c;
+		System.out.println("The results of Int Operations are "+opr1+" , "+opr2+" , "+opr3+" and "+opr4);
+	}
+}
